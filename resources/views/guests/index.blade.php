4349e9d4e90c2edce6478221a4484263feef09c6
swaran <webxpertdeveloper@gmail.com>
2026-01-12 15:08:30 +0530

    Fixed width

--- resources/views/guests/index.blade.php
+++ resources/views/guests/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="p-4 md:p-6">
-    <div class="max-w-7xl mx-auto">
+    <div class="max-w mx-auto">
         <!-- Header -->
         <div class="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4 mb-6">
             <div>
